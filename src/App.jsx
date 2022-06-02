b9d71ae9e1d9418284f5d74fb2a9b9ef4875d9cf
rabosian <msann1356@gmail.com>
2022-06-02 14:47:50 -0400

    finish styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const addItem = () => {
     if (!inputValue) {
       alert('Enter an item')
-      return 
+      return
     }
 
     const item = {
@@ -27,21 +27,29 @@ function App() {
 
   return (
     <main>
-      <div className='header'>
-        <h1>Todo List</h1>
-        <input
-          type="text"
-          placeholder='enter todos'
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)} />
-        <button className='add-btn' onClick={addItem}>Add</button>
-      </div>
+      <div className='container'>
+        <div className='header'>
+          <h1>Todo List</h1>
+        </div>
+        <div className='form'>
+          <input
+            type="text"
+            placeholder='enter todos'
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)} />
+          <button className='add-btn' onClick={addItem}>
+            <span>Add</span>
+          </button>
+        </div>
 
-      <ul>
-        {items.map(item => {
-          return <li key={item.id}>{item.value} <button className='delete-btn' onClick={() => deleteItem(item.id)}>❌</button></li>
-        })}
-      </ul>
+        <div>
+          <ul>
+            {items.map(item => {
+              return <li key={item.id}>{item.value} <button className='delete-btn' onClick={() => deleteItem(item.id)}>❌</button></li>
+            })}
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
