5e78c4bcb607df0c3abebb03d9ea737178a44572
rabosian <msann1356@gmail.com>
2022-06-02 13:04:56 -0400

    finish without components

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import TodoItem from './components/TodoItem';
 
 
 function App() {
   const [inputValue, setInputValue] = useState('')
-  const [todoList, setTodoList] = useState([])
+  const [items, setItems] = useState([])
 
   const addItem = () => {
-    setTodoList([...todoList, inputValue])
+    if (!inputValue) {
+      alert('Enter an item')
+      return 
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: inputValue
+    }
+    setItems(prev => [...prev, item])
     setInputValue('')
   }
 
   const deleteItem = (id) => {
-    setTodoList((prev) => {
-      prev.filter((item,index) => {
-        return index !== id
-      })
-    })
+    const newList = items.filter(item => item.id !== id)
+    setItems(newList)
   }
 
   return (
     <main>
       <div className='header'>
         <h1>Todo List</h1>
-        <input value={inputValue} type="text" placeholder='enter some text' onChange={(event)=> setInputValue(event.target.value)}/>
+        <input
+          type="text"
+          placeholder='enter todos'
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)} />
         <button className='add-btn' onClick={addItem}>Add</button>
       </div>
-      <div>
-        {todoList.map((item, index) => {
-          return <TodoItem 
-            key={index}
-            id={index}
-            item={item}
-            deleteItem={deleteItem}
-          />
+
+      <ul>
+        {items.map(item => {
+          return <li key={item.id}>{item.value} <button className='delete-btn' onClick={() => deleteItem(item.id)}>❌</button></li>
         })}
-      </div>
+      </ul>
     </main>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,22 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 
 function TodoItem(props) {
-    const [isDone, setIsDone] = useState(false)
-    return (
-        <div 
-            className="todo-item"
-            // onClick={()=>{
-            //     props.deleteItem(props.id)
-            // }}
-            onClick={()=>{
-                setIsDone(prev => !prev)
-            }}
-            style={{textDecoration: isDone && "strike-through"}}
-        >
-        {props.item}
-        </div>
-    )
+
 }
 
 export default TodoItem
